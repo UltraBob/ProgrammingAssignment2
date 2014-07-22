7c1220ed52b5f4a2da18c54e1c8a96170a6363af
Bob McDonald <bob.tdlc@gmail.com>
2014-07-22 21:39:54 +0900

    first pass at making a working version of the homework assignment, likely to be modified after more testing, out fo time for now

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions take the sometimes time-consuming task of
+## calculating the inverse of a matrix, and ensure that while
+## the underlying matrix remains unchanged, the calculation
+## of the inverse need only happen once.
 
-## Write a short comment describing this function
+## This function takes a matrix as an argument, and contains
+## getters and setters for both the matrix itself, and the
+## inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() {
+        x
+    }
+    setInverse <- function(invertedMatrix) {
+        inverse <<- invertedMatrix
+    }
+    getInverse <- function() {
+        inverse
+    }
+    list(set = set, get = get, 
+         setInverse = setInverse, 
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function takes an object created with makeCacheMatrix
+## as an argument and returns the inverse of its matrix, from
+## cache if it exists, and via calculation that is then cached
+## if not.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    invertedMatrix <- x$getInverse()
+    if(!is.null(invertedMatrix)) {
+        message("getting cached data")
+        return(invertedMatrix)
+    }
+    data <- x$get()
+    invertedMatrix <- solve(data)
+    x$setInverse(invertedMatrix)
+    invertedMatrix
 }
